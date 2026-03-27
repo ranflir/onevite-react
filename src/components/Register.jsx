17b0d23d974528ef2726a6cfa6aa9a3de8ed2c44
KIMHYUNJE <ranflir.sc@gmail.com>
2026-03-27 17:45:37 +0900

    feat: State로 사용자 입력 관리하기2

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,37 +3,76 @@
 import { useState } from 'react';
 
 const Register = () => {
-  const [name, setName] = useState('name');
-  const [birth, setBirth] = useState('');
-  const [country, setCountry] = useState('');
-  const [bio, setBio] = useState('');
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const [input, setInput] = useState({
+    name: '',
+    birth: '',
+    country: '',
+    bio: '',
+  });
+  // const [name, setName] = useState('name');
+  // const [birth, setBirth] = useState('');
+  // const [country, setCountry] = useState('');
+  // const [bio, setBio] = useState('');
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
+  // const onChangeName = (e) => {
+  //   // setName(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     // spread 연산자를 써야 다른거 안날아감.
+  //     name: e.target.value,
+  //   });
+  // };
 
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
+  // const onChangeBirth = (e) => {
+  //   // setBirth(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     birth: e.target.value,
+  //   });
+  // };
 
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  };
+  // const onChangeCountry = (e) => {
+  //   // setCountry(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     country: e.target.value,
+  //   });
+  // };
+
+  // const onChangeBio = (e) => {
+  //   // setBio(e.target.value);
+  //   setInput({
+  //     ...input,
+  //     bio: e.target.value,
+  //   });
+  // };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder="name" />
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder="name"
+        />
       </div>
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value=""></option>
 
           <option value="kr">Korea</option>
@@ -42,7 +81,7 @@ const Register = () => {
         </select>
       </div>
       <div>
-        <textarea value={bio} onChange={onChangeBio} />
+        <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
     </div>
   );
